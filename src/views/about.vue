0db28433adf54cc89a40018ca9f51719c605475b
zeynepozbek2@gmail.com <Zeynep.94>
2022-02-06 18:11:54 +0300

    router

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>About HEREEEE!!!</div>
+  <b-container class="dynamic-page">
+    <img src="https://capechristian.com/wp-content/uploads/2018/12/Banner-movies.jpg" alt="">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam autem, consequatur, consequuntur cumque
+      dolorum fugiat ipsam laudantium minus modi molestiae odit perspiciatis quam quos tenetur totam voluptate. Dignissimos, mollitia.
+    </p>
+  </b-container>
 </template>
+
+<style lang="scss">
+.dynamic-page {
+  img {
+    margin-bottom: 16px;
+    width: 100%;
+    height: auto;
+  }
+  p {
+    color: white;
+  }
+}
+</style>
